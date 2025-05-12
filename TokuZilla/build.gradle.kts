9ab510ead6491e6f23ffdea75763054fe3b08649
Mohd Kaif Shaikh <63385299+KaifTaufiq@users.noreply.github.com>
2025-05-12 05:44:39 +0530

    Create build.gradle.kts

---
+++ TokuZilla/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    authors = listOf("Kaif Shaikh")
+    description = "Tokuzilla all in one Site for Tokusatsu"
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "Cartoon"
+    )
+
+    iconUrl = ""
+
+    isCrossPlatform = true
+}
