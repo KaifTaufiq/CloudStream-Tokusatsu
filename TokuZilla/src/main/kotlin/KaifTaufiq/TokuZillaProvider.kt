0cc18e579a808faed92c439c8ffb55b2c16a61f2
Mohd Kaif Shaikh <63385299+KaifTaufiq@users.noreply.github.com>
2025-05-12 14:29:34 +0530

    Update TokuZillaProvider.kt

--- TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaProvider.kt
+++ TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaProvider.kt
@@ -5,12 +5,12 @@ import com.lagradost.cloudstream3.*
 class TokuZillaProvider : MainAPI() {
   override var mainUrl = "https://tokuzilla.net/"
   override var name = "TokuZilla"
-  override var supportedTypes = setOf(
+  override val supportedTypes = setOf(
     TvType.Cartoon,
     TvType.Anime,
     TvType.AnimeMovie,
     TvType.Movie,
   )
   override val hasMainPage = true
-  override val lang = "en"
+  override var lang = "en"
 }
