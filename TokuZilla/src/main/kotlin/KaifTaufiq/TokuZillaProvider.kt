0e114fc929b8b7f0b25f6f9aab9e5dd93f8468e6
Mohd Kaif Shaikh <63385299+KaifTaufiq@users.noreply.github.com>
2025-05-12 15:02:42 +0530

    Update TokuZillaProvider.kt

--- TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaProvider.kt
+++ TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaProvider.kt
@@ -13,4 +13,13 @@ class TokuZillaProvider : MainAPI() {
   )
   override val hasMainPage = true
   override var lang = "en"
+
+  override val mainPage = mainPageof(
+    "/categories/super-sentai" = "Super Sentai",
+    "/categories/kamen-rider" = "Kamen Rider",
+    "/anime" = "Tokusatsu Anime",
+    "/series" = "Anime",
+    "/movie" = "Movie",
+  )
+  
 }
