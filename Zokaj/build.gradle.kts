126fbfb8ab9c949092c34a250247094d78084f29
Mohd Kaif Shaikh <63385299+KaifTaufiq@users.noreply.github.com>
2025-05-12 17:07:13 +0530

    Update build.gradle.kts

--- Zokaj/build.gradle.kts
+++ Zokaj/build.gradle.kts
@@ -1,1 +1,24 @@
+version = 1
 
+cloudstream {
+    language = "en"
+    authors = listOf("Kaif Shaikh")
+    description = "Zokaj all in one Site for Tokusatsu"
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "Cartoon"
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/KaifTaufiq/CloudStream-Tokusatsu/refs/heads/master/TokuZilla/icon.png"
+
+    isCrossPlatform = true
+}
