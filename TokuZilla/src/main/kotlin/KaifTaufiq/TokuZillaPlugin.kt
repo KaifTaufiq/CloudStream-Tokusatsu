778795eebd996df6df240e17064b24f5ab0fcdfa
kaiftaufiq <kaifshaikhtaufiq@gmail.com>
2025-05-14 12:58:16 +0530

    Done

--- TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaPlugin.kt
+++ TokuZilla/src/main/kotlin/KaifTaufiq/TokuZillaPlugin.kt
@@ -2,6 +2,7 @@ package com.kaiftaufiq
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.BoosterX
 import android.content.Context
 
 @CloudstreamPlugin
@@ -9,5 +10,6 @@ class TokuZillaPlugin: Plugin() {
   override fun load(context: Context) {
     // All providers should be added in this manner. Please don't edit the providers list directly.
     registerMainAPI(TokuZillaProvider())
+    registerExtractorAPI(BoosterX())
   }
 }
